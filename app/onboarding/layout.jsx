b93420bc70a8f55b27a33c8c828cca308f33b888
Savidya Sathyani <mails.savidya@gmail.com>
2023-07-28 21:04:01 +1000

    Complete onboarding common components

--- app/onboarding/layout.jsx
+++ app/onboarding/layout.jsx
@@ -1,4 +1,5 @@
 import OnboardingBackground from '@/components/OnboardingBackground';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'BusyBee | Login',
@@ -8,7 +9,18 @@ const OnboardingLayout = ({ children }) => {
   return (
     <section className="grid-1-row-center onboarding">
       <div className="grid-2-row-center canvas">
-        <div className="form">{children}</div>
+        <div className="form">
+          <div className="logo">
+            <Image
+              className="ml-8"
+              src={'/assets/icons/logo-white.png'}
+              alt="Busy Bee"
+              width={200}
+              height={100}
+            />
+          </div>
+          <div className="ob-content">{children}</div>
+        </div>
         <OnboardingBackground />
       </div>
     </section>
